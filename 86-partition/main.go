b24dfae565c4f8eccd0573fd28e45419c08160d9
ole <liuzunxiong@qq.com>
2021-11-28 19:06:09 +0800

    86 519

--- 86-partition/main.go
+++ 86-partition/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -23,6 +25,7 @@ func partition(head *ListNode, x int) *ListNode {
 		}
 		head = head.Next
 	}
+	tb.Next = nil
 	ts.Next = big.Next
 	if sma.Next != nil {
 		return sma.Next
@@ -31,5 +34,20 @@ func partition(head *ListNode, x int) *ListNode {
 }
 
 func main() {
-
+	bg1 := &ListNode{1, nil}
+	bg2 := &ListNode{4, nil}
+	bg3 := &ListNode{3, nil}
+	bg4 := &ListNode{2, nil}
+	bg5 := &ListNode{5, nil}
+	bg6 := &ListNode{2, nil}
+	bg1.Next = bg2
+	bg2.Next = bg3
+	bg3.Next = bg4
+	bg4.Next = bg5
+	bg5.Next = bg6
+	t := partition(bg1, 3)
+	for t != nil {
+		fmt.Println(t.Val)
+		t = t.Next
+	}
 }
