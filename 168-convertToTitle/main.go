81502fed1cf59e5181b69fc971c1a64784eddb1a
ole <liuzunxiong@qq.com>
2021-12-20 04:19:47 +0800

    #168

---
+++ 168-convertToTitle/main.go
@@ -0,0 +1,15 @@
+package main
+
+func convertToTitle(columnNumber int) string {
+	res := ""
+	for columnNumber > 0 {
+		columnNumber--
+		res = string(columnNumber%26+'A') + res
+		columnNumber /= 26
+	}
+	return res
+}
+
+func main() {
+
+}
