d0f7d7f81cf3ad8834e3b307dcbe917b32eb80e3
ole <liuzunxiong@qq.com>
2021-12-30 00:45:00 +0800

    #1296

---
+++ 1296-isPossibleDivide/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "sort"
+
+func isPossibleDivide(hand []int, groupSize int) bool {
+	mp := make(map[int]int)
+	l := len(hand)
+	if l%groupSize != 0 {
+		return false
+	}
+	for i := 0; i < l; i++ {
+		mp[hand[i]]++
+	}
+	ks := []int{}
+	for k, _ := range mp {
+		ks = append(ks, k)
+	}
+	sort.Ints(ks)
+	//fmt.Println(ks)
+	for _, i := range ks {
+		if mp[i] > 0 {
+			t := mp[i]
+			for j := i; j < i+groupSize; j++ {
+				mp[j] -= t
+				if mp[j] < 0 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+
+}
